436ddfdad969df2029e3bc10d659819100e881d7
yunanajesse211 <69218648+yunanajesse211@users.noreply.github.com>
2023-12-30 14:07:40 +0100

    Update youtube_app.py

--- youtube_app.py
+++ youtube_app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 from pytube import YouTube
-import os
 
 class YouTubeDownloader:
     def __init__(self):
+        st.set_page_config(page_title='JC Youtube downloader', page_icon=':tv')
         st.title("YouTube Video Downloader")
         self.url = st.text_input("Enter YouTube Video URL:")
-        self.resolutions = ["2160p", "1440p", "1080p", "720p", "360p", "240p"]
+        self.resolutions = ["2160p","1440p","1080p","720p", "360p", "240p"]
         self.resolution = st.selectbox("Select Resolution:", self.resolutions)
         self.download_button = st.button("Download Video")
+        self.progress_bar = st.progress(0.0)
 
     def download_video(self):
         try:
@@ -17,15 +18,8 @@ class YouTubeDownloader:
                 video = yt.streams.filter(res=self.resolution).first()
                 if video:
                     st.success(f"Downloading: {yt.title} ({self.resolution})")
-
-                   
-                    # If the user doesn't choose a path, use the default download directory
-                    download_folder =os.path.expanduser("~")
-
-                    # Download with progress callback
-                    video.download(output_path=download_folder, filename="video", on_progress_callback=lambda stream, chunk, remaining: self.on_progress(stream, chunk, remaining))
-
-                    st.success(f"Download completed successfully! Video saved at: {os.path.join(download_folder, 'video.mp4')}")
+                    video.download()
+                    st.success("Download completed successfully!")
                 else:
                     st.error("No video with the specified resolution found.")
             else:
@@ -37,11 +31,14 @@ class YouTubeDownloader:
         total_size = stream.filesize
         bytes_downloaded = total_size - remaining
         percentage = (bytes_downloaded / total_size) * 100.0
-        st.progress(percentage)
+        self.progress_bar.progress(percentage)
 
     def run(self):
         if self.download_button:
-            self.download_video()
+            # Clear progress bar
+            self.progress_bar.empty()
+            with st.spinner("Downloading in progress..."):
+                self.download_video()
 
 if __name__ == "__main__":
     downloader = YouTubeDownloader()
